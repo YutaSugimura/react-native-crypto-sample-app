4da9aa28b1abee7b04359d2a62d7c92ac92f42a7
YutaSugimura <nextc3m@gmail.com>
2022-08-03 11:18:22 +0900

    feat: implemented using react-native-quick-crypto

--- App.tsx
+++ App.tsx
@@ -8,110 +8,64 @@
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
+import React, {useCallback, useState} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
-  View,
 } from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Crypto from 'react-native-quick-crypto';
+import {ethers} from 'ethers';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App = () => {
+const App: React.FC = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const [address, setAddress] = useState<string>();
+
+  const onCreate = useCallback(() => {
+    const start = (global as any).performance.now();
+
+    const wallet = ethers.Wallet.createRandom();
+    setAddress(wallet.address);
+
+    const end = (global as any).performance.now();
+    console.log(`Creating a Wallet took ${end - start} ms.`);
+  }, []);
+
+  const onGenerateHash = useCallback(() => {
+    const hashed = Crypto.createHash('sha256')
+      .update('Hello world')
+      .digest('hex');
+    console.log(hashed);
+  }, []);
+
+  const onReset = useCallback(() => {
+    setAddress(undefined);
+  }, []);
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+        style={backgroundStyle}
+      />
+      <Text>address: {address}</Text>
+
+      <Button title="Create Wallet" onPress={onCreate} />
+      <Button title="Reset" onPress={onReset} />
+      <Button title="Generate Hash" onPress={onGenerateHash} />
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
